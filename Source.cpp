6bbc4a84a6f4a0d4c42a5b2c3ef16b5afff68763
Thomas Walford <20815153+twalford@users.noreply.github.com>
2018-08-20 16:04:21 +1000

    Added Panning
    Added ability to pan the screen.
    Off-screen windows show a green line on nearest side of screen.
    Screen now only draws on mouse event.

--- Source.cpp
+++ Source.cpp
@@ -7,6 +7,10 @@ public:
 	{
 	}
 
+	~olcWindow() 
+	{
+	}
+
 	short x = 3;
 	short y = 3;
 	short width = 30;
@@ -27,7 +31,18 @@ private:
 	{
 		for (auto s = ws.begin(); s != ws.end(); s++) 
 		{
-			if (std::next(s) == ws.end())
+			// Check if outside of screen
+			if (s->x + s->width + panOffsetX < 0) // to the left
+				DrawLine(0, max(s->y + panOffsetY, 0), 0, min(s->y + s->height + panOffsetY, scrHght_1), PIXEL_HALF, FG_GREEN);
+			else if (s->x + panOffsetX > scrWdth_1) // to the right
+				DrawLine(scrWdth_1, max(s->y + panOffsetY, 0), scrWdth_1, min(s->y + s->height + panOffsetY, scrHght_1), PIXEL_HALF, FG_GREEN);
+			else if (s->y + s->height + panOffsetY < 0) // above
+				DrawLine(s->x + panOffsetX, 0, s->x + s->width + panOffsetX, 0, PIXEL_HALF, FG_GREEN);
+			else if (s->y + panOffsetY > scrHght_1) // below
+				DrawLine(s->x + panOffsetX, scrHght_1, s->x + s->width + panOffsetX, scrHght_1, PIXEL_HALF, FG_GREEN);
+
+			// not outside, try to draw the whole window
+			else if (std::next(s) == ws.end())
 				DrawWindow(*s, PIXEL_SOLID, FG_WHITE); 
 			else
 				DrawWindow(*s, PIXEL_HALF, FG_GREY);
@@ -36,30 +51,46 @@ private:
 
 	void DrawWindow(olcWindow &w, short glyph, short colour)
 	{
+		// Adjust window position for panning
+		w.x += panOffsetX;
+		w.y += panOffsetY;
+
 		// Fill background of window
 		Fill(w.x + 1, w.y + 1, w.x + w.width, w.y + w.height, PIXEL_SOLID, FG_BLACK);
 
 		// Draw lines of window
-		DrawLine(w.x, w.y, w.x + w.width, w.y, glyph, colour); //top
-		DrawLine(w.x, w.y + w.headerSize, w.x + w.width, w.y + w.headerSize, glyph, colour); //header bottom
-		DrawLine(w.x, w.y + w.height, w.x + w.width, w.y + w.height, glyph, colour); //bottom
-
 		DrawLine(w.x, w.y, w.x, w.y + w.height, glyph, colour); //left
 		DrawLine(w.x + w.width, w.y, w.x + w.width, w.y + w.height, glyph, colour); //right
+
+		DrawLine(w.x + 1, w.y, w.x + w.width - 1, w.y, glyph, colour); //top
+		DrawLine(w.x + 1, w.y + w.headerSize, w.x + w.width - 1, w.y + w.headerSize, glyph, colour); //header bottom
+		DrawLine(w.x + 1, w.y + w.height, w.x + w.width - 1, w.y + w.height, glyph, colour); //bottom
+
 	}
 
 public:
 	std::vector<olcWindow> windows;
 	olcWindow myFirstWindow;
 
-	short offsetX = 0;
-	short offsetY = 0;
+	int scrWdth_1;
+	int scrHght_1;
+
+	short dragOffsetX = 0;
+	short dragOffsetY = 0;
+	short panOffsetX = 0;
+	short panOffsetY = 0;
+
+	const short maxPan = 1000;
+
 	bool resizing = false;
 	bool dragging = false;
+	bool panning = false;
 
 	bool OnUserCreate() override
 	{
 		Fill(0, 0, ScreenWidth(), ScreenHeight(), PIXEL_SOLID, FG_BLACK);
+		scrWdth_1 = ScreenWidth() - 1;
+		scrHght_1 = ScreenHeight() - 1;
 
 		windows.push_back(myFirstWindow);
 		DrawWindows(windows);
@@ -69,10 +100,11 @@ public:
 
 	bool OnUserUpdate(float fElapsedTime) override
 	{
-		Fill(0, 0, ScreenWidth(), ScreenHeight(), PIXEL_SOLID, FG_BLACK);
-
-		CheckMouseEvent();
-		DrawWindows(windows);
+		if (CheckMouseEvent())
+		{
+			Fill(0, 0, ScreenWidth(), ScreenHeight(), PIXEL_SOLID, FG_BLACK);
+			DrawWindows(windows);
+		}
 
 		return true;
 	}
@@ -88,36 +120,36 @@ public:
 
 	bool IsWithinWindow(int i)
 	{
-		return  m_mousePosX >= windows[i].x &&
-				m_mousePosX <= windows[i].x + windows[i].width &&
-				m_mousePosY >= windows[i].y &&
-				m_mousePosY <= windows[i].y + windows[i].height;
+		return  m_mousePosX - panOffsetX >= windows[i].x &&
+				m_mousePosX - panOffsetX <= windows[i].x + windows[i].width &&
+				m_mousePosY - panOffsetY >= windows[i].y &&
+				m_mousePosY - panOffsetY <= windows[i].y + windows[i].height;
 	}
 
 	void CreateNewWindow()
 	{
 		olcWindow tempWnd;
-		tempWnd.x = m_mousePosX;
-		tempWnd.y = m_mousePosY;
+		tempWnd.x = m_mousePosX - panOffsetX;
+		tempWnd.y = m_mousePosY - panOffsetY;
 		windows.push_back(tempWnd);
 	}
 
 	void ResizeActiveWindow()
 	{
-		if (m_mousePosY - windows.back().y > windows.back().headerSize)
-			windows.back().height = m_mousePosY - windows.back().y;
+		if (m_mousePosY - panOffsetY - windows.back().y > windows.back().headerSize)
+			windows.back().height = m_mousePosY - panOffsetY - windows.back().y;
 
-		if (m_mousePosX - windows.back().x > 10)
-			windows.back().width = m_mousePosX - windows.back().x;
+		if (m_mousePosX - panOffsetX - windows.back().x > 10)
+			windows.back().width = m_mousePosX - panOffsetX - windows.back().x;
 	}
 
 	void SetPositionActiveWindow()
 	{
-		windows.back().x = m_mousePosX - offsetX;
-		windows.back().y = m_mousePosY - offsetY;
+		windows.back().x = m_mousePosX - dragOffsetX;
+		windows.back().y = m_mousePosY - dragOffsetY;
 	}
 
-	void CheckMouseEvent()
+	bool CheckMouseEvent()
 	{
 		bool foundWindow = false;
 		bool deleteWindow = false;
@@ -127,17 +159,29 @@ public:
 		{
 			resizing = false;
 			dragging = false;
+			panning = false;
 		}
 
 		//// Dragging ////
 		if (dragging)
+		{
 			SetPositionActiveWindow();
+			return true;
+		}
 
 		//// Resizing ////
 		if (resizing)
 		{
 			ResizeActiveWindow();
-			return;
+			return true;
+		}
+
+		//// Panning ////
+		if (panning)
+		{
+			panOffsetX = max(min(m_mousePosX - dragOffsetX, maxPan),-maxPan);
+			panOffsetY = max(min(m_mousePosY - dragOffsetY, maxPan),-maxPan);
+			return true;
 		}
 
 		//// Right Mouse Button ////
@@ -145,7 +189,10 @@ public:
 		{
 			// Create new Window if there is none.
 			if (windows.size() == 0)
+			{
 				CreateNewWindow();
+				return true;
+			}
 			// Otherwise loop through the windows.
 			else
 			{
@@ -156,11 +203,14 @@ public:
 					{
 						SetWindowActive(i);
 						windows.pop_back();
-						return;
+						return true;
 					}
 					// Create a window if one isn't clicked.
 					else if (i == 0)
+					{
 						CreateNewWindow();
+						return true;
+					}
 				}
 			}
 		}
@@ -168,44 +218,47 @@ public:
 		//// Left mouse Button ////
 		if (m_mouse[0].bPressed)
 		{
-			// Return if no windows.
-			if (windows.size() <= 0)
-				return;
-
-			// Otherwise loop through the windows.
+			// Loop through the windows.
 			for (int i = windows.size() - 1; i >= 0; i--)
 			{
 				// Check if clicked bottom right pixel.
-				if (m_mousePosX == windows[i].x + windows[i].width && m_mousePosY == windows[i].y + windows[i].height)
+				if (m_mousePosX - panOffsetX == windows[i].x + windows[i].width && m_mousePosY - panOffsetY == windows[i].y + windows[i].height)
 				{
 					// RESIZE
 					SetWindowActive(i);
 					resizing = true;
-					return;
+					return true;
 				}
 
 				// Check if click in header.
-				if (m_mousePosX >= windows[i].x &&
-					m_mousePosX <= windows[i].x + windows[i].width &&
-					m_mousePosY >= windows[i].y &&
-					m_mousePosY <= windows[i].y + windows[i].headerSize)
+				if (m_mousePosX - panOffsetX >= windows[i].x &&
+					m_mousePosX - panOffsetX <= windows[i].x + windows[i].width &&
+					m_mousePosY - panOffsetY >= windows[i].y &&
+					m_mousePosY - panOffsetY <= windows[i].y + windows[i].headerSize)
 				{
 					// DRAG
-					offsetX = m_mousePosX - windows[i].x;
-					offsetY = m_mousePosY - windows[i].y;
+					dragOffsetX = m_mousePosX - windows[i].x;
+					dragOffsetY = m_mousePosY - windows[i].y;
 					SetWindowActive(i);
 					dragging = true;
-					return;
+					return true;
 				}
 
 				// Check if clicked in any part of window.
 				if (IsWithinWindow(i))
 				{
 					SetWindowActive(i);
-					return;
+					return true;
 				}
 			}
+
+			// Panning
+			dragOffsetX = m_mousePosX - panOffsetX;
+			dragOffsetY = m_mousePosY - panOffsetY;
+			panning = true;
+			return true;
 		}
+		return false;
 	}
 };
 
